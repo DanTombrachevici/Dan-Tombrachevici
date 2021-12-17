e519792c2af41a8f2b58fe9fcf8776028d9ed762
DanTombrachevici <75080623+DanTombrachevici@users.noreply.github.com>
2021-12-17 15:08:19 +0200

    Add files via upload

---
+++ Laborator 6 Tweb Tombrachevici Dan/Games.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Games</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <section class="bg">
+     <h1 class="text_title">Games</h1>
+    <ul>
+        <li><a href="https://store.steampowered.com/app/1252330/DEATHLOOP/">Deathloop (2021)</a></li>
+        <li><a href="https://store.steampowered.com/app/546560/HalfLife_Alyx/">Half-Life: Alyx(2020)</a></li>
+        <li><a href="https://store.steampowered.com/app/730/CounterStrike_Global_Offensive/">Counter-Strike: Global Offensive(2012)</a></li>
+      </ul>
+      <div class="loading-text"></div>
+    
+      <script src="js/script.js"></script>
+    <p><a href="index.php">Back to menu</a></p>
+    <p class="working">In working...</p>
+  </section>
+</body>
+</html>
